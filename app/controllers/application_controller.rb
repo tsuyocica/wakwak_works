b8218236f1a67b95b5760100b80aeb0524931d0c
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-29 22:06:18 +0900

    userモデルにアソシエーション、バリデーションを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  # Deviseのストロングパラメータ設定
+  def configure_permitted_parameters
+    added_keys = [:username, :full_name, :furigana, :birth_date, :experience]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_keys)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Deviseの設定
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+# アソシエーション
+has_many :job_posts, dependent: :destroy  # 施工管理者が作成した案件
+has_many :job_applications, dependent: :destroy  # 作業員が応募した案件
+
+# バリデーション
+validates :username, presence: true
+validates :full_name, presence: true
+validates :furigana, presence: true
 end
