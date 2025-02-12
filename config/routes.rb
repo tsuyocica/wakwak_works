f5baac1721406b7904c814a167c6057f845f441f
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-02-12 09:41:17 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable' # ✅ WebSocketのエンドポイントを設定
+
   devise_for :users, controllers: {
     registrations: "users/registrations" # カスタムコントローラーを適用
   }
