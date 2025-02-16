451c35c23f3e906f422b52c518999559e2fc8550
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-02-16 16:56:17 +0900

    Revert "コンフリクト解消"
    This reverts commit 25aed6178ee4a53a51c1304c1288caf1a5f2f074.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,11 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-<<<<<<< Updated upstream
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-=======
->>>>>>> Stashed changes
 //= link_tree ../videos
-//= link_tree ../../javascript/channels .js
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
