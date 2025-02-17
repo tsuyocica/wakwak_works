1ca514e67b23f709bccefe989509d7284b3ca70c
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-02-17 11:20:29 +0900

    開発環境のRedisの設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  ENV["REDIS_URL"] ||= ENV["REDIS_URL_EXTERNAL"]
+  ENV["REDIS_URL"] ||= "redis://localhost:6379/1"
 end
