1ab5d84f0b4362280edf1cd8da92fc0fd917f0bf
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-02-18 23:43:44 +0900

    各ページ微調整

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,14 +5,7 @@ import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 // ✅ Stimulus アプリケーションの作成
 const application = Application.start();
 eagerLoadControllersFrom("./controllers", application);
-window.Stimulus = application;
 
-console.log("✅ Stimulus is running:", application);
-console.log(
-  "✅ Registered Controllers:",
-  application.router.modulesByIdentifier
-);
-console.log("✅ チャンネルインデックスが読み込まれました！");
 // ✅ Turbo & Bootstrap の読み込み
 import "@hotwired/turbo-rails";
 import "controllers";
